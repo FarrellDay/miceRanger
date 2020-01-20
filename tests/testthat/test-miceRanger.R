57436c0b339bdf945ba7e8558e3d9aae49d6f05a
samFarrellDay <sam@farrellday.com>
2020-01-20 14:56:00 -0500

    Added Complex Scenario

---
+++ tests/testthat/test-miceRanger.R
@@ -0,0 +1,21 @@
+context("miceRanger - complex specifications")
+
+data(iris)
+ampDat <- amputeData(iris)
+
+v <- list(
+  Sepal.Width = c("Sepal.Length","Petal.Width","Species")
+  , Sepal.Length = c("Sepal.Width","Petal.Width")
+  , Species = c("Sepal.Width")
+)
+pmm <- c(
+    Sepal.Width = "meanMatch"
+  , Sepal.Length = "value"
+  , Species = "meanMatch"
+)
+
+miceObj <- miceRanger(
+  ampDat
+  , vars = v
+  , valueSelector = pmm
+)
