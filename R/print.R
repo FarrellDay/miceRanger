b3551cb67aae6b4f0cb34af8ece940cbd9f7e19b
samFarrellDay <sam@farrellday.com>
2020-01-24 10:19:42 -0500

    tabs to spaces

--- R/print.R
+++ R/print.R
@@ -10,13 +10,13 @@ print.miceDefs <- function(x,...) {
   m <- x$callParams$m
   maxiter <- x$callParams$maxiter
   imputationTime <- x$imputationTime
-  cat("\nClass:\t\t miceDefs\n")
-  cat("Datasets:\t",m,"\n")
-  cat("Iterations:\t",maxiter,"\n")
-  cat("Total Seconds:\t",imputationTime,"Seconds","\n")
-  cat("Imputed Cols:\t",length(x$callParams$vars),"\n")
-  cat("Estimated Time per Additional Iteration is",round(imputationTime/m),"Seconds","\n")
-  cat("Estimated Time per Additional Dataset is",round(imputationTime/maxiter),"Seconds","\n\n")
+  cat("\nClass:          miceDefs\n")
+  cat("Datasets:      ",m,"\n")
+  cat("Iterations:    ",maxiter,"\n")
+  cat("Total Seconds: ",imputationTime,"\n")
+  cat("Imputed Cols:  ",length(x$callParams$vars),"\n")
+  cat("Estimated Time per Additional Iteration is",round(imputationTime/maxiter*m),"Seconds","\n")
+  cat("Estimated Time per Additional Dataset is",round(imputationTime/m*maxiter),"Seconds","\n\n")
   cat("For additional metrics, see the different plotting functions.")
   invisible(x)
 }
