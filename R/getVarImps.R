363a3f2bd6ce1be9b7b2e580a376bc8eccb7dc2a
samFarrellDay <sam@farrellday.com>
2020-01-28 19:48:13 -0500

    Added impDefs to S3

--- R/getVarImps.R
+++ R/getVarImps.R
@@ -1,19 +1,60 @@
-#' @title getVarImps
-#' @description Returns all of the imputations for the specified
-#' datasets and variable.
-#' @param miceObj A miceDefs object created by \code{miceRanger}.
+#' Get Variable Imputations
+#' 
+#' These functions exist solely to get at the imputed data for a
+#' specific dataset and variable.
+#' 
+#' @name getVarImps
+#' @description Returns imputations for the specified datasets and variable.
+#' @param x A \code{miceDefs} or \code{impDefs} object.
 #' @param datasets The datasets to return. Can be a number, of a numeric vector.
 #' @param var The variable to return the imputations for.
 #' @return A matrix of imputations for a single variable. 
 #' Each column represents a different dataset.
 #' @examples
 #' data("sampleMiceDefs")
-#' getVarImps(sampleMiceDefs,"Sepal.Width")
+#' getVarImps(sampleMiceDefs,var="Sepal.Width")
 #' @export
-getVarImps <- function(
-    miceObj
-  , datasets = 1:miceObj$callParams$m
+getVarImps <- function(x,datasets,var) UseMethod("getVarImps",x)
+
+#' @export
+getVarImps.default <- function(x,datasets,var) {
+  stop("x must be of miceDefs or impDefs class.")
+}
+
+#' @method getVarImps miceDefs
+#' @export
+getVarImps.miceDefs <- function(
+    x
+  , datasets = 1:x$callParams$m
   , var
 ) {
-  as.matrix(sapply(miceObj$finalImps[datasets],function(x) x[[var]]))
+  if (missing(var)) stop("Please provide a variable with the 'var' parameter")
+  return(
+    as.matrix(
+      sapply(
+          x$finalImps[datasets]
+        , function(i) i[[var]]
+      )
+    )
+  )
+}
+
+#' @method getVarImps impDefs
+#' @export
+getVarImps.impDefs <- function(
+    x
+  , datasets = x$callParams$datasets
+  , var
+) {
+  if (missing(var)) stop("Please provide a variable with the 'var' parameter")
+  return(
+    as.matrix(
+      sapply(
+        x$imputedData[paste0("Dataset_",datasets)]
+        , function(i) {
+          i[x$naWhere[,var],var,with=FALSE][[1]]
+        }
+      )
+    )
+  )
 }
--- R/print.R
+++ R/print.R
@@ -10,7 +10,7 @@ print.miceDefs <- function(x,...) {
   m <- x$callParams$m
   maxiter <- x$callParams$maxiter
   imputationTime <- x$imputationTime
-  cat("\nClass:          miceDefs\n")
+  cat("Class:          miceDefs\n")
   cat("Datasets:      ",m,"\n")
   cat("Iterations:    ",maxiter,"\n")
   cat("Total Seconds: ",imputationTime,"\n")
@@ -18,5 +18,17 @@ print.miceDefs <- function(x,...) {
   cat("Estimated Time per Additional Iteration is",round(imputationTime/maxiter*m),"Seconds","\n")
   cat("Estimated Time per Additional Dataset is",round(imputationTime/m*maxiter),"Seconds","\n\n")
   cat("For additional metrics, see the different plotting functions.")
+  invisible(x)
+}
+
+#' @export
+print.impDefs <- function(x,...) {
+  m <- x$callParams$datasets
+  iterations <- x$callParams$iterations
+  cat("Class:          impDefs\n")
+  cat("Datasets:      ",m,"\n")
+  cat("Iterations:    ",iterations,"\n")
+  cat("Data Rows:     ",nrow(x$imputedData[[1]]),"\n")
+  cat("Data Cols:     ",ncol(x$imputedData[[1]]),"\n")
   invisible(x)
 }
