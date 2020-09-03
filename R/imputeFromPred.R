3b6153e792430d7761927df50b49c6b45c879e7d
Byron <byron.jaeger@gmail.com>
2020-09-03 14:24:59 -0500

    classification impute for 1 NA value

--- R/imputeFromPred.R
+++ R/imputeFromPred.R
@@ -8,18 +8,27 @@ imputeFromPred <- function(
   , priorPreds
 )
 {
-  
+
   # pred - The output from the model of the samples you want to impute
   # modelType - Classification or Regression
   # valueSelector - meanMatch or value
   # meanMatchCandidates - Integer
   # prior - Unaltered values of original nonmissing data
   # priorPreds model predictions associated with prior
-  
+
   if (valueSelector == "value") {
     return(pred)
   } else {
     if (modelType == "Classification") {
+
+      # boundary case: if a categorical variables has only one missing
+      # value, the pred object will be a vector instead of a matrix.
+      if(!is.matrix(pred)){
+        pred_names <- names(pred)
+        pred <- matrix(pred, nrow = 1)
+        colnames(pred) <- pred_names
+      }
+
       lvls <- colnames(pred)
       return(apply(pred,MARGIN=1,function(x) sample(lvls,prob=x,size=1)))
     } else if (modelType == "Regression") {
